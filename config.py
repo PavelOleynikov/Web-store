c5f97719784f09ed4c20c1005a1459e048e4d456
Pavel Oleynikov <Pavlushka19805@gmail.com>
2025-12-11 12:05:29 +0400

    created web_app.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+DATA_DIR = BASE_DIR / "html_pages"
+PATH_TO_HTML = DATA_DIR / "contacts.html"
---
+++ web_app.py
@@ -0,0 +1,46 @@
+# Импорт встроенной библиотеки для работы веб-сервера
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from config import PATH_TO_HTML
+
+# настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
+    """
+
+    def do_GET(self) -> None:
+        """Метод для обработки входящих GET-запросов"""
+        try:
+            self.send_response(200)  # Отправка кода ответа
+            self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+            self.end_headers()  # Завершение формирования заголовков ответа
+            with open(PATH_TO_HTML, "r", encoding="utf-8") as file_html:
+                self.wfile.write(bytes(file_html.read().encode()))  # Тело ответа
+
+        except FileNotFoundError:
+            self.send_error(404, "File not found")  # Отправка кода ответа и сообщения об ошибке
+        except Exception as e:
+            self.send_error(500, f"Internal server error: {str(e)}")
+
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу MyServer
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Старт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
